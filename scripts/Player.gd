a87074d59e4a175b8b12bee7f5f57bcb20a51cca
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-02-20 22:31:27 +0100

    New Movement
    slowed the movement
    added coyote time
    and jump buffer

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 
-const GRAVITY = -100
-const MOVE_SPEED = 400
-const JUMP_FORCE = 1500
+const GRAVITY = -50
+const MOVE_SPEED = 250
+const JUMP_FORCE = 800
 
+var jumpwasPressed = false
+var coyote_time = true
 var direction=0
 var velocity= Vector2.ZERO
-var move_mode =1
+var move_mode = 1
 var jump=false
 var button_check = true
 
@@ -19,29 +21,39 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("change"):
 		move_mode= move_mode*-1
-	if event.is_action_pressed("jump") and is_on_floor():
-		jump=true
 	if event.is_action_pressed("pause"):
 		get_tree().paused = false
 
 
 func _physics_process(_delta):
-
 	
-	if velocity.x==0:
+	if is_on_floor():
+		if jumpwasPressed:
+			velocity.y-=JUMP_FORCE
+		else:
+			if Input.is_action_just_pressed("jump"):
+				velocity.y-=JUMP_FORCE
+		coyote_time =true
+		
+	if is_on_floor()==false:
+			coyotetime()
+			if  Input.is_action_just_pressed("jump"):
+				jumpwasPressed=true
+				rememberJumptime()
+	print(jumpwasPressed)
+	
+	if velocity.x==0 and is_on_floor():
 		move_mode=move_mode*-1
 	if move_mode ==1:
 		velocity.x =MOVE_SPEED
 	else:
 		velocity.x =-MOVE_SPEED
-		
-	if jump == true:
-		velocity.y-=JUMP_FORCE
-		jump=false
-		
 	velocity.y -= GRAVITY
+
+
 	
 	velocity=move_and_slide(velocity,Vector2.UP)
+
 	
 
 func _on_Hurtbox_area_entered(area):
@@ -54,3 +66,12 @@ func _on_Hurtbox_area_entered(area):
 			get_parent().get_node("Gate-Button").get_node("Gate").get_node("Hitbox").queue_free()
 			get_parent().get_node("Gate-Button").get_node("Gate").get_node("ani_sprite").queue_free()
 			button_check=false
+
+func coyotetime():
+	yield(get_tree().create_timer(0.2),"timeout")
+	coyote_time = false
+
+func rememberJumptime():
+	yield(get_tree().create_timer(0.2),"timeout")
+	jumpwasPressed = false
+	
